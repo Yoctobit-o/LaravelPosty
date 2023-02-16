6dce4e9b7ef30f36b5cf155fe119d599ec09bdee
ISMAIL AMAAN <kefey6iaa@nottingham.edu.my>
2023-02-16 17:08:33 +0500

    pagination, listing posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     //
     public function index(){
-        return view('posts.index');
+        $posts = Post::paginate(2); // returns all posts in natural database order //laravel collection
+
+     //   return view('posts.index', [
+     //       'posts' => $posts
+     //   ]);
+
+        return view('posts.index', compact('posts'));
     }
 
     public function store(Request $request){
